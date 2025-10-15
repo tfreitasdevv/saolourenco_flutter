c9bab039bdfc39ca55baef36a692d8f2ca2fdc75
Thiago Fonseca de Freitas <tfreitasdevv@gmail.com>
2025-10-15 18:26:32 -0300

    Voltando os ícones ao tamanho menor anterior

--- lib/app/modules/home/widgets/icons_home.dart
+++ lib/app/modules/home/widgets/icons_home.dart
@@ -15,34 +15,26 @@ class IconsHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.blue.withOpacity(0.0), // Cor de fundo com transparência
-        borderRadius: BorderRadius.circular(10), // Bordas arredondadas
-      ),
-      child: IconButton(
-        padding: EdgeInsets.zero, // Remove o padding padrão
-        constraints: BoxConstraints(), // Remove as constraints padrão
-        icon: web == false
-            ? CachedNetworkImage(
+    return IconButton(
+      icon: web == false
+          ? CachedNetworkImage(
+              color: Colors.white,
+              imageUrl: icone,
+              errorWidget: (context, url, error) => Icon(
+                Icons.image_not_supported,
                 color: Colors.white,
-                imageUrl: icone,
-                errorWidget: (context, url, error) => Icon(
-                  Icons.image_not_supported,
-                  color: Colors.white,
-                ),
-              )
-            : Image.network(
-                icone,
-                color: Colors.white,
-                errorBuilder: (context, error, stackTrace) => Icon(
-                  Icons.image_not_supported,
-                  color: Colors.white,
-                ),
               ),
-        iconSize: 20,
-        onPressed: funcao,
-      ),
+            )
+          : Image.network(
+              icone,
+              color: Colors.white,
+              errorBuilder: (context, error, stackTrace) => Icon(
+                Icons.image_not_supported,
+                color: Colors.white,
+              ),
+            ),
+      iconSize: 20,
+      onPressed: funcao,
     );
   }
 }
