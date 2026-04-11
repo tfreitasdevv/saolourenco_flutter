49dc005c1d57494b47fc9ff0197258ea0f9f3437
Thiago Fonseca de Freitas <tfreitasdevv@gmail.com>
2026-04-11 17:01:55 -0300

    Dados da página EAC agora é populado diamicamente através de maps no firebase, sendo necessário colocar o texto:string e ordem:int64 em cada map. O nome do map é o título. Se o título for "_", o título não é exibido, somente seu texto correspondente.

--- lib/app/modules/eac/eac_page.dart
+++ lib/app/modules/eac/eac_page.dart
@@ -2,19 +2,43 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../../shared/constants/constants.dart';
+import '../../shared/widgets/rich_text_markdown.dart';
 
 class EacPage extends StatefulWidget {
   final String title;
-  const EacPage({Key? key, this.title = "EAC"})
-      : super(key: key);
+  const EacPage({Key? key, this.title = "EAC"}) : super(key: key);
 
   @override
   _EacPageState createState() => _EacPageState();
 }
 
 class _EacPageState extends State<EacPage> {
+  /// Extrai as seções do documento Firebase e ordena pelo campo "ordem"
+  List<Map<String, dynamic>> _extrairSecoes(DocumentSnapshot snapshot) {
+    final data = snapshot.data() as Map<String, dynamic>? ?? {};
+    final secoes = <Map<String, dynamic>>[];
+
+    for (final entry in data.entries) {
+      if (entry.value is Map) {
+        final map = entry.value as Map<String, dynamic>;
+        secoes.add({
+          'titulo': entry.key,
+          'texto': (map['texto'] ?? '').toString(),
+          'ordem': (map['ordem'] ?? 999) is int
+              ? map['ordem']
+              : int.tryParse(map['ordem'].toString()) ?? 999,
+        });
+      }
+    }
+
+    secoes.sort((a, b) => (a['ordem'] as int).compareTo(b['ordem'] as int));
+    return secoes;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isWide = MediaQuery.of(context).size.width > 400;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: t2,
@@ -27,7 +51,7 @@ class _EacPageState extends State<EacPage> {
             image: DecorationImage(image: AssetImage(bg), fit: BoxFit.cover)),
         child: SafeArea(
           child: SingleChildScrollView(
-          child: FutureBuilder<DocumentSnapshot>(
+            child: FutureBuilder<DocumentSnapshot>(
               future: FirebaseFirestore.instance
                   .collection('conteudo_pagina_pastoral')
                   .doc('eac')
@@ -38,80 +62,55 @@ class _EacPageState extends State<EacPage> {
                     child: CircularProgressIndicator(),
                   );
                 }
-                String contato = snapshot.data!["contato"];
-                String contatoF = contato.replaceAll("\\n", "\n");
-                String texto = snapshot.data!["texto"];
-                String textoF = texto.replaceAll("\\n", "\n");
+
+                final secoes = _extrairSecoes(snapshot.data!);
+
+                if (secoes.isEmpty) {
+                  return Container(
+                    padding: EdgeInsets.all(28),
+                    child: Center(
+                      child: Text(
+                        'Nenhum conteúdo disponível.',
+                        style: TextStyle(
+                          fontSize: isWide ? 18 : 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  );
+                }
+
                 return Container(
                   padding: EdgeInsets.all(28),
                   child: Column(
-                    children: <Widget>[
-                      Container(
-                        child: Text(
-                          textoF,
-                          style: TextStyle(
-                              fontSize: MediaQuery.of(context).size.width > 400
-                                  ? 18
-                                  : 16,
-                              color: Colors.white),
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      for (int i = 0; i < secoes.length; i++) ...[
+                        if (secoes[i]['titulo'] != '_') ...[
+                          Text(
+                            secoes[i]['titulo'],
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: isWide ? 22 : 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                        ],
+                        RichTextMarkdown(
+                          markdownText: secoes[i]['texto'],
+                          fontSize: isWide ? 18 : 16,
+                          textColor: Colors.white,
                           textAlign: TextAlign.justify,
                         ),
-                      ),
-                      SizedBox(height: 22),
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              "Coordenação",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize:
-                                      MediaQuery.of(context).size.width > 400
-                                          ? 22
-                                          : 20,
-                                  color: Colors.white),
-                            ),
-                            Text(
-                              snapshot.data!["coordenacao"],
-                              style: TextStyle(
-                                  fontSize:
-                                      MediaQuery.of(context).size.width > 400
-                                          ? 18
-                                          : 16,
-                                  color: Colors.white),
-                            ),
-                            SizedBox(
-                              height: 12,
-                            ),
-                            Text(
-                              "Contato",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize:
-                                      MediaQuery.of(context).size.width > 400
-                                          ? 22
-                                          : 20,
-                                  color: Colors.white),
-                            ),
-                            Text(
-                              contatoF,
-                              style: TextStyle(
-                                  fontSize:
-                                      MediaQuery.of(context).size.width > 400
-                                          ? 18
-                                          : 16,
-                                  color: Colors.white),
-                            )
-                          ],
-                        ),
-                      ),
+                        if (i < secoes.length - 1) SizedBox(height: 22),
+                      ],
                       SizedBox(height: 22),
                     ],
                   ),
                 );
-              }),
+              },
+            ),
           ),
         ),
       ),
